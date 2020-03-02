ed7ef165fc2d9c56368064c23000ff3275a435d4
tdownes57 <tomdownes1@gmail.com>
2020-03-02 00:11:26 -0800

    Added the project LinkedLists_Core.

---
+++ LinkedLists_Core/LinkedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+//
+//Added 3/2/2020 thomas downes
+//
+
+namespace LinkedLists_Core
+{
+    public class LinkedList
+    {
+        //
+        //Added 3/2/2020 thomas downes
+        //
+        LinkedValue mod_firstItem;  
+
+        public LinkedList(int param_FirstItem)
+        {
+            //
+            //Added 3/2/2020 thomas downes
+            //
+            mod_firstItem = new LinkedValue(param_FirstItem);
+
+        }
+
+        public void Add(int param_NextItem)
+        {
+            //
+            //Added 3/2/2020 thomas downes
+            //
+            mod_firstItem.PassValue_ToEndOfList(param_NextItem);
+
+        }
+
+        public int Count()
+        {
+            //
+            //Added 3/2/2020 td 
+            //
+            int intCount = (mod_firstItem == null ? 0 : 1); 
+                
+            mod_firstItem.CountItemsInChain(ref intCount);
+
+            return intCount;  
+
+        }
+
+    }
+}
---
+++ LinkedLists_Core/LinkedValue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//
+//Added 3/2/2020 thomas downes
+//
+namespace LinkedLists_Core
+{
+    class LinkedValue
+    {
+        //
+        //Added 3/2/2020 thomas downes
+        //
+        public int Value = 0;
+        public LinkedValue NextValue;
+
+        public LinkedValue(int param_value)
+        {
+            Value = param_value; 
+
+        }
+
+        public void PassValue_ToEndOfList(int param_value)
+        {
+            if (NextValue == null)
+            {
+                NextValue = new LinkedValue(param_value);
+            }
+            else
+            {
+                NextValue.PassValue_ToEndOfList(param_value);
+            }
+
+        }
+
+        public void CountItemsInChain(ref int par_intCount)
+        {
+            //
+            //Added 3/2/2020  thomas downes  
+            //
+            if (NextValue != null)
+            {
+                par_intCount++;  
+                NextValue.CountItemsInChain(ref par_intCount);
+            }
+
+        }
+
+    }
+}
