f8792827948b6ea9fe676203356316d2cc849d7c
tdownes57 <tomdownes1@gmail.com>
2020-03-09 14:05:34 -0700

    Added commenting for comprehension.

--- InterviewKickstart_Sorting/Program.cs
+++ InterviewKickstart_Sorting/Program.cs
@@ -14,172 +14,169 @@ namespace InterviewKickstart_Sorting
          * 
          * Merge_K_sorted_arrays
 
+        Problem Statement:
 
+        This is a popular facebook problem.
 
-Problem Statement:
+        Given K sorted arrays arr, of size N each, merge them into a new array res, such that res is a sorted array.
 
 
+        Assume N is very large compared to K. N may not even be known. The arrays could be just sorted streams, for instance, timestamp streams.
 
-This is a popular facebook problem.
 
-Given K sorted arrays arr, of size N each, merge them into a new array res, such that res is a sorted array.
 
+        All arrays might be sorted in increasing manner or decreasing manner. Sort all of them in the manner they appear in input.
 
-Assume N is very large compared to K. N may not even be known. The arrays could be just sorted streams, for instance, timestamp streams.
 
 
+        Note:
 
-All arrays might be sorted in increasing manner or decreasing manner. Sort all of them in the manner they appear in input.
+        Repeats are allowed.
+        Negative numbers and zeros are allowed.
+        Assume all arrays are sorted in the same order. Preserve that sort order in output.
+        It is possible to find out the sort order from at least one of the arrays.
 
 
+        Input/Output Format For The Function:
 
-Note:
 
-Repeats are allowed.
-Negative numbers and zeros are allowed.
-Assume all arrays are sorted in the same order. Preserve that sort order in output.
-It is possible to find out the sort order from at least one of the arrays.
 
+        Input Format:
 
-Input/Output Format For The Function:
 
 
+        There is only one argument: 2D Integer array arr.
 
-Input Format:
+        Here, arr[i][j] denotes value at index j of ith input array, 0-based indexing. So, arr is K * N size array.
 
 
 
-There is only one argument: 2D Integer array arr.
+        Output Format:
 
-Here, arr[i][j] denotes value at index j of ith input array, 0-based indexing. So, arr is K * N size array.
 
 
+        Return an integer array res, containing all elements from all individual input arrays combined.
 
-Output Format:
 
 
+        Input/Output Format For The Custom Input:
 
-Return an integer array res, containing all elements from all individual input arrays combined.
 
 
+        Input Format:
 
-Input/Output Format For The Custom Input:
 
 
+        The first line of input should contain an integer K. The second line should contain an integer N, denoting size of each input array.
 
-Input Format:
+        In next K lines, ith line should contain N space separated integers, denoting content of ith array of K input arrays, where jth element in this ith line is nothing but arr[i][j], i.e. value at index j of ith array, 0-based indexing.  
 
 
 
-The first line of input should contain an integer K. The second line should contain an integer N, denoting size of each input array.
+        If K = 3, N = 4 and arr = [
 
-In next K lines, ith line should contain N space separated integers, denoting content of ith array of K input arrays, where jth element in this ith line is nothing but arr[i][j], i.e. value at index j of ith array, 0-based indexing.  
+        [1, 3, 5, 7],
 
+                    [2, 4, 6, 8],
 
+                    [0, 9, 10, 11]
 
-If K = 3, N = 4 and arr = [
+        ], then input should be:
 
-[1, 3, 5, 7],
 
-            [2, 4, 6, 8],
 
-            [0, 9, 10, 11]
+        3
 
-], then input should be:
+        4
 
+        1 3 5 7
 
+        2 4 6 8
 
-3
+        0 9 10 11
 
-4
 
-1 3 5 7
 
-2 4 6 8
+        Output Format:
 
-0 9 10 11
 
 
+        There will be (N*K) lines of output, where ith line contains an integer res[i], denoting value at index i of res.
 
-Output Format:
+        Here, res is the result array returned by solution function.
 
 
 
-There will be (N*K) lines of output, where ith line contains an integer res[i], denoting value at index i of res.
+        For input K = 3, N = 4 and arr = [
 
-Here, res is the result array returned by solution function.
+        [1, 3, 5, 7],
 
+                    [2, 4, 6, 8],
 
+                    [0, 9, 10, 11]
 
-For input K = 3, N = 4 and arr = [
+        ], output will be:
 
-[1, 3, 5, 7],
 
-            [2, 4, 6, 8],
 
-            [0, 9, 10, 11]
+        0
 
-], output will be:
+        1
 
+        2
 
+        3
 
-0
+        4
 
-1
+        5
 
-2
+        6
 
-3
+        7
 
-4
+        8
 
-5
+        9
 
-6
+        10
 
-7
+        11
 
-8
 
-9
 
-10
+        Constraints:
 
-11
+        1 <= N <= 500
+        1 <= K <= 500
+        -10^6 <= arr[i][j] <= 10^6, for all valid i,j
 
+        Sample Test Case:
 
+        Sample Input:   K = 3, N =  4
 
-Constraints:
+        arr[][] = { {1, 3, 5, 7},
 
-1 <= N <= 500
-1 <= K <= 500
--10^6 <= arr[i][j] <= 10^6, for all valid i,j
+                   {2, 4, 6, 8},
 
-Sample Test Case:
+                   {0, 9, 10, 11}} ;
 
-Sample Input:   K = 3, N =  4
+        Sample Output:
 
-arr[][] = { {1, 3, 5, 7},
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-           {2, 4, 6, 8},
-
-           {0, 9, 10, 11}} ;
-
-Sample Output:
-
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
-Note:   A solution which dumps all elements from all arrays into one massive heap, and then extracts the elements one by one into a sorted output may pass, but is not acceptable. These issues should go away when we have a more nuanced backend.
+        Note:   A solution which dumps all elements from all arrays into one massive heap, and then extracts the elements one by one into a sorted output may pass, but is not acceptable. These issues should go away when we have a more nuanced backend.
 
          * 
          * 
          * 
         * Complete the mergeArrays function below.
+        * 
          */
         static int[] mergeArrays(int[][] arr)
         {
             /*
-             * Write your code here.
+             *   Solution by Thomas Downes. 
              */
 
             short shNumArraysK = (short)arr.GetLength(0);
@@ -206,7 +203,7 @@ Note:   A solution which dumps all elements from all arrays into one massive hea
             //Output. 
             int[] res = new int[shNumArraysK * shNumItemsN];
 
-            int intLastOutputMaxOrMin = -1; //  arr[0][0];
+            //int intLastOutputMaxOrMin = -1; //  arr[0][0];
             short shCurrentResIndex = 0;
             bool bKeepLooping = true;
 
@@ -220,6 +217,7 @@ Note:   A solution which dumps all elements from all arrays into one massive hea
             do
             {
                 bAllArraysHaveBeenExhausted = true;  //Initialize to True. 
+                bool bFirstUnexhaustedArrayBeingChecked = true;   
 
                 for (short shArrayIndexI = 0; shArrayIndexI <= -1 + shNumArraysK; shArrayIndexI++)
                 {
@@ -235,14 +233,28 @@ Note:   A solution which dumps all elements from all arrays into one massive hea
                     bAllArraysHaveBeenExhausted = false;  //The default of True is ___NOT___ the valid truth.  
                     intArrayCurrentValue = arr[shArrayIndexI][shCurrentArray_Pointer];
 
-                    if (shArrayIndexI == 0)
+                    if (bFirstUnexhaustedArrayBeingChecked) //(shArrayIndexI == 0)
                     {
+                        //
+                        // We have to initialize the "Provisional" variables. 
+                        //
                         intProvisionalOutput_value = intArrayCurrentValue;
                         shProvisionalOutput_itemIndex = pointerArrayIndexes[shArrayIndexI];
                         shProvisionalOutput_arrayIndex = shArrayIndexI;
                     }
                     else
                     {
+                        //
+                        // We have to compare the provisional integer value (vs. short values)
+                        //   to the current array value.
+                        //
+                        // The variable name bSmaller... is for comprehension purposes, the 
+                        //    variaable is named as if we know all the arrays are in 
+                        //    ascending order (1, 234, 2399, etc.).   Howver, the 
+                        //    expression on the right-hand side does not make that
+                        //    assumption (see use of var. boolAscendingArrays).
+                        //   ----3/9/2020 thomas downes
+                        //
                         bSmallerThanProvisional = (boolAscendingArrays ? (intArrayCurrentValue < intProvisionalOutput_value) :
                                                                          (intArrayCurrentValue > intProvisionalOutput_value));
                         if (bSmallerThanProvisional)
@@ -253,6 +265,13 @@ Note:   A solution which dumps all elements from all arrays into one massive hea
                         }
 
                     }
+
+                    //
+                    //Prepare for the next iteration of the For-Next loop. 
+                    //
+                    bFirstUnexhaustedArrayBeingChecked = false; //In the 2nd & subsequent iterations, ... 
+                    //  ... this Boolean should be false.  
+
 
                 }
 
@@ -265,6 +284,8 @@ Note:   A solution which dumps all elements from all arrays into one massive hea
                 shCurrentResIndex++;
                 
                 //Indicate that we have pulled a value from one of the arrays. 
+                //   (This is the first time I've used the ++ operator on an 
+                //   array item.   It should work okay.)
                 pointerArrayIndexes[shProvisionalOutput_arrayIndex]++;
 
                 //for (int intArrayIndexKL = 0; intArrayIndexKL < )
@@ -276,8 +297,6 @@ Note:   A solution which dumps all elements from all arrays into one massive hea
             return res;  
 
     }
-
-
 
 
 
