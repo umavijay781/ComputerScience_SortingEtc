1819e94965c0e13b51ff0f3f80a08267edb852b4
tdownes57 <tomdownes1@gmail.com>
2020-03-15 14:32:40 -0700

    Renamed InterviewKickstart_Python from ......_HeapQueue.

--- ComputerScience_Sorting_Lib/SortingByQuickSort.cs
+++ ComputerScience_Sorting_Lib/SortingByQuickSort.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -36,9 +36,81 @@ namespace ComputerScience_Sorting_Lib
             //    https://sorting.at   
             //
 
+
+
+
+
+
+
             return 0;
 
 
         }
-     }
- }
+
+        private void Partituing_by_Hoare(int[] param_array, int par_indexOfPivot)
+        {
+            //
+            // Renamed from "Sort_InPlace_ByPivotIndex".  
+            //
+            // Added 3/12/2020 thomas downes 
+            //
+            //      Let's use Lamuto's Partitioning / Two-Pointer Approach   
+            //
+            if (1 == param_array.Length) return;
+
+            int intPivotValue = param_array[par_indexOfPivot];
+            int intCurrentPivotIndex = par_indexOfPivot;
+            //int intInnerPointer = 0;
+            bool bLeftOfPivot;
+            bool bRightOfPivot;
+
+            //
+            //  Ironically, we take the pivot __out__ of the bulk of the array. 
+            //
+            //  Put the Pivot at the leftmost lefthand position of the array. 
+            //
+            int intTempValue = param_array[0];
+            param_array[0] = param_array[intCurrentPivotIndex];
+            param_array[intCurrentPivotIndex] = intTempValue;
+            intCurrentPivotIndex = 0; 
+
+            //
+            //  Let's begin Hoare's Partioning.   
+            //
+            //  Hoare's Partitioning uses two pointers which begin at opposite sides of the 
+            //  array.
+            //
+            //  The pivot is now at Index 0, so our loop below will skip tha 0t                    
+            //
+            for (int intPointerIndex = 1; intPointerIndex <= -1 + param_array.Length; intPointerIndex++)
+            {
+                //
+                // Avoid the pivot itself. 
+                //
+                if (intPointerIndex == intCurrentPivotIndex) continue;
+
+                //
+                // Hoare's Partitioning???   
+                //
+                //  Hoare's Partitioning uses two pointers which begin at opposite sides of the 
+                //  array.  
+                //
+                bLeftOfPivot = (intPointerIndex < intCurrentPivotIndex);
+                bRightOfPivot = (intPointerIndex > intCurrentPivotIndex);
+
+                //
+                //  
+                //
+
+
+
+
+
+
+            }
+
+            return;
+
+        }
+    }
+}
