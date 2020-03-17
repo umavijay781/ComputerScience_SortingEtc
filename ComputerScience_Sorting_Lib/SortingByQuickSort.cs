bf12bc6be0a069f06c641b39af788d1f66ba277e
tdownes57 <tomdownes1@gmail.com>
2020-03-17 04:13:18 -0700

    I have completed Hoare's partitioning.

--- ComputerScience_Sorting_Lib/SortingByQuickSort.cs
+++ ComputerScience_Sorting_Lib/SortingByQuickSort.cs
@@ -47,7 +47,7 @@ namespace ComputerScience_Sorting_Lib
 
         }
 
-        private void Partituing_by_Hoare(int[] param_array, int par_indexOfPivot)
+        public void Partitioning_by_Hoare(int[] param_array, int par_indexOfPivot)
         {
             //
             // Renamed from "Sort_InPlace_ByPivotIndex".  
@@ -69,10 +69,14 @@ namespace ComputerScience_Sorting_Lib
             //
             //  Put the Pivot at the leftmost lefthand position of the array. 
             //
+            //      (Swap the Pivot Value at the position 0.) 
+            //
             int intTempValue = param_array[0];
             param_array[0] = param_array[intCurrentPivotIndex];
             param_array[intCurrentPivotIndex] = intTempValue;
-            intCurrentPivotIndex = 0; 
+
+            //intCurrentPivotIndex = 0;
+            bool bKeepLooping = true;
 
             //
             //  Let's begin Hoare's Partioning.   
@@ -82,12 +86,20 @@ namespace ComputerScience_Sorting_Lib
             //
             //  The pivot is now at Index 0, so our loop below will skip tha 0t                    
             //
-            for (int intPointerIndex = 1; intPointerIndex <= -1 + param_array.Length; intPointerIndex++)
+            // for (int intPointerIndex = 1; intPointerIndex <= -1 + param_array.Length; intPointerIndex++)
+
+            int intPointerIndex_I = 1;
+            int intPointerIndex_K = -1 + param_array.Length;
+            bool bNeedToThrow_LeftToRight = false;
+            bool bNeedToThrow_RightToLeft = false; 
+
+            do
             {
                 //
-                // Avoid the pivot itself. 
+                //  ---*---This was taken care of, by swapping with item #0.---
+                //  ---*--Skip/avoid the pivot itself. 
                 //
-                if (intPointerIndex == intCurrentPivotIndex) continue;
+                // if (intPointerIndex == intCurrentPivotIndex) continue;
 
                 //
                 // Hoare's Partitioning???   
@@ -95,20 +107,83 @@ namespace ComputerScience_Sorting_Lib
                 //  Hoare's Partitioning uses two pointers which begin at opposite sides of the 
                 //  array.  
                 //
-                bLeftOfPivot = (intPointerIndex < intCurrentPivotIndex);
-                bRightOfPivot = (intPointerIndex > intCurrentPivotIndex);
+                // bLeftOfPivot = (intPointerIndex < intCurrentPivotIndex);
+                // bRightOfPivot = (intPointerIndex > intCurrentPivotIndex);
 
                 //
-                //  
+                //Find the first lefthand item which should be "thrown" to the right,
+                //   i.e. from left side to the right side). 
                 //
+                do
+                {
+                    intPointerIndex_I++;
+
+                    //Unlikely to occur....
+                    if (intPointerIndex_I > -1 + param_array.Length) break;
+
+                    bNeedToThrow_LeftToRight = (param_array[intPointerIndex_I] > intPivotValue);
+
+                    if (intPointerIndex_I > intPointerIndex_K) break;
+
+                } while (false == bNeedToThrow_LeftToRight);
 
 
+                //Have the two pointers croseed each other? 
+                bKeepLooping = (intPointerIndex_I < intPointerIndex_K);
+                if (false == bKeepLooping) break;
 
 
+                //
+                //Find the first righthand item which should be "thrown" to the left (i.e. right-to-left). 
+                //
+                do
+                {
+                    intPointerIndex_K--;
+                    
+                    //Unlikely to occur....
+                    if (intPointerIndex_K < 1) break;
 
+                    bNeedToThrow_RightToLeft = (param_array[intPointerIndex_K] < intPivotValue);
 
-            }
+                    //if (intPointerIndex_K < intPointerIndex_I) break;
+                    bKeepLooping = (intPointerIndex_I < intPointerIndex_K);
+                    if (bKeepLooping) break;
 
+                } while (false == bNeedToThrow_LeftToRight);
+
+                //Unlikely to occur....
+                bKeepLooping = (intPointerIndex_I <= -1 + param_array.Length);
+                if (false == bKeepLooping) break;
+                //Unlikely to occur....
+                bKeepLooping = (intPointerIndex_K >= 1);
+                if (false == bKeepLooping) break;
+
+                //Have the two pointers croseed each other? 
+                bKeepLooping = (intPointerIndex_I < intPointerIndex_K);
+                if (false == bKeepLooping) break;
+
+                //
+                //  Swap / Throw
+                //
+                int intThrow_LeftToRight = param_array[intPointerIndex_I];
+                int intThrow_RightToLeft = param_array[intPointerIndex_K];
+                
+                param_array[intPointerIndex_I] = intThrow_RightToLeft;
+                param_array[intPointerIndex_K] = intThrow_LeftToRight;
+
+            } while (bKeepLooping);
+
+            //
+            //Place the Pivot Value where it should go, i.e. somewhere
+            //   in the midst of the array (vs. in the 0th position). 
+            //
+            int intLessThanPivot = param_array[intPointerIndex_K];
+            param_array[intPointerIndex_K] = intPivotValue;
+            param_array[0] = intLessThanPivot;
+
+            //
+            //We have completed our work.  
+            //
             return;
 
         }
