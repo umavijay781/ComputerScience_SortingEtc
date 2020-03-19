94bc774b0825f5a05673fbeda6cf254f2d3f2303
tdownes57 <tomdownes1@gmail.com>
2020-03-19 12:38:16 -0700

    I iterate through the last several rows, in case the next adjacent row doesn't allow any Queens.

--- InterviewKickstart_HeapQueue/ClassNQueensSD.py
+++ InterviewKickstart_HeapQueue/ClassNQueensSD.py
@@ -1,7 +1,7 @@
 class ClassNQueensSD():
-        # Added 3/18/2020 thomas downes
+    # Added 3/18/2020 thomas downes
     """description of class"""
-      #
+    #
     # N Queens Problem
     #     
     #   SD = Subproblem Definition  
@@ -18,8 +18,8 @@ class ClassNQueensSD():
         self.N_numRows = N_size ## self.N_GeneralProblemSize ## 2 ## Initialize.
         self.N_numColumns = N_size ## It is a square board. 
         
-        self.D_numDiagonalsLR = (-1 + 2 * self.N_numRows) ## LR = //// diagonals slash-forward //// 
-        self.D_numDiagonalsRL = (-1 + 2 * self.N_numRows) ## RL = \\\\ diagonals slash-backward \\\\
+        self.D_numDiagonalsLR = (-1 + (2 * self.N_numRows)) ## LR = //// diagonals slash-forward //// 
+        self.D_numDiagonalsRL = (-1 + (2 * self.N_numRows)) ## RL = \\\\ diagonals slash-backward \\\\
 
         self.RowIndex = 0
         self.ColumnsSelected_EachRow = []   ## [-1, -1, -1, -1, -1, -1, -1, -1]
@@ -116,7 +116,8 @@ class ClassNQueensSD():
         #     setColumnChoice_isItOkay_RowCol
         #
         boolIsOkay_NoAttacks = self.setColumnChoice_isItOkay_RowCol(self.RowIndex, k_column)
-        self.RowIndex += 1
+        ##--/----For the incrementation, see "(par_iRowIndex + 1)" in module moduleNQueens.py. 
+        ##--/--self.RowIndex += 1
         return boolIsOkay_NoAttacks
 
     def setColumnChoice_isItOkay_RowCol(self, i_row, k_column):
@@ -141,7 +142,7 @@ class ClassNQueensSD():
                Ex3 = ValueError()
                Ex3.strerror = "This || column is already taken!!"
                raise Ex3
-               return false
+               ##return false
             self.ColumnsSelected_EachRow[i_row] = k_column
             self.ColumnIsUsed[k_column] = True
             # 
@@ -153,7 +154,7 @@ class ClassNQueensSD():
                Ex2 = ValueError()
                Ex2.strerror = "This // diagonal is already taken!!"
                raise Ex2
-               return false
+               ##return false
             self.DiagonalsForward_1Queen[i_row + k_column] = True # Indicate that this forward-slash diagonal // is now occupied.
             #
             # \\\ \\\ \\\ \\\ \\\
@@ -165,33 +166,53 @@ class ClassNQueensSD():
                Ex1 = ValueError()
                Ex1.strerror = "This \\ diagonal is already taken!!"
                raise Ex1
-               return False
+               ##return False
             self.DiagonalsBackslash_1Queen[i_row + k_column_inverse] = True # Indicate that this backslash diagonal \\ is now occupied. 
+            ## Feedback to programmer
+            strRowLine = self.ToString_Row(i_row)
+            print("The following is partial feedback: " + strRowLine) ## Provide feedback.
             return True
 
         
-    def removeLastColumnChoice(self):
+    def removeLastColumnChoice(self, par_i_row):
         # Added 3/18/2020 thomas downes
         #
         # Remove the last prior column choice.
         #
-        i_row = self.RowIndex  
-        k_column = self.ColumnsSelected_EachRow[i_row]
-        # |||||||||||||||||||||
-        self.ColumnsSelected_EachRow[i_row] = -1 ## Re-initialize.
-        # /// /// /// /// ///
-        self.DiagonalsForward_1Queen[i_row + k_column] = False # Indicate that this forward-slash diagonal // is __not occupied. 
-        # \\\ \\\ \\\ \\\ \\\
-        k_column_inverse = (self.N_numColumns - k_column - 1) # Subtract 1 to make the Column Inverse 0-based instead of 1-based.
-        self.DiagonalsBackslash_1Queen[i_row + k_column_inverse] = False # Indicate that this backslash diagonal \\ is __not occupied. 
-        self.RowIndex += -1  # Decrement the Row Index.
+        ##---i_row = self.RowIndex  
+        if (par_i_row > (-1 + self.N_numRows)):
+            #
+            # Pass, since we don't need to do anything. 
+            #
+            pass
+        else:
+            #
+            # Query for the selected column.
+            #
+            k_column = self.ColumnsSelected_EachRow[par_i_row]
+            #
+            # If (k_column == 1) then the following is _NOT_ needed.
+            #
+            if (k_column != -1):
+                # |||||||||||||||||||||
+                self.ColumnsSelected_EachRow[par_i_row] = -1 ## Re-initialize.
+                # /// /// /// /// ///
+                self.DiagonalsForward_1Queen[par_i_row + k_column] = False # Indicate that this forward-slash diagonal // is __not occupied. 
+                # \\\ \\\ \\\ \\\ \\\
+                k_column_inverse = (self.N_numColumns - k_column - 1) # Subtract 1 to make the Column Inverse 0-based instead of 1-based.
+                self.DiagonalsBackslash_1Queen[par_i_row + k_column_inverse] = False # Indicate that this backslash diagonal \\ is __not occupied. 
+                ##--/----For the incrementation, see "(par_iRowIndex + 1)" in module moduleNQueens.py. 
+                ##--/--self.RowIndex += -1  # Decrement the Row Index.
         return 
 
 
     def Completed(self):
        # return (self.RowIndex >= (-1 + self.N_numRows))
        # return (self.RowIndex >= 2)
-       return (self.RowIndex >= 3)
+       # return (self.RowIndex >= 3)
+       # return (self.RowIndex >= (-1 + self.N_numRows))
+       return (self.RowIndex > (-1 + self.N_numRows))
+
 
     def Output_Array(self):
         # Added 3/18/2020 thomas downes
@@ -209,15 +230,31 @@ class ClassNQueensSD():
         output_row = ""
         for i_row in range(self.N_numRows):
             output_row = ""
-            for k_col in range(self.N_numColumns):
-                if (self.ColumnsSelected_EachRow[i_row] == k_col):
-                    output_row = (output_row + "q")
-                else:
-                    output_row = (output_row + "-")
+            #for k_col in range(self.N_numColumns):
+            #    #if (self.ColumnsSelected_EachRow[i_row] == k_col):
+            #    #    output_row = (output_row + "q")
+            #    #else:
+            #    #    output_row = (output_row + "-")
+            output_row = self.ToString_Row(i_row)
             output_strings.append(output_row)
         return output_strings
 
-    def Output_MasterString(self):
+
+    def ToString_Row(self, par_i_row):
+        ##
+        ## Converts a chess row to a string value.
+        ##
+        output_row = ""
+        intColumnSelected = self.ColumnsSelected_EachRow[par_i_row]
+        for k_col in range(self.N_numColumns):
+            if (intColumnSelected == k_col):
+                output_row = (output_row + "q")
+            else:
+                output_row = (output_row + "-")
+        return output_row
+
+
+    def Output_ChessboardString(self):
         # Added 3/18/2020 thomas downes
         #
         # This returns the following string with carriage returns:
--- InterviewKickstart_HeapQueue/moduleNQueens.py
+++ InterviewKickstart_HeapQueue/moduleNQueens.py
@@ -14,19 +14,26 @@ def GetSolutions_NQueens_Overall(N_RowsColumnsQueens):
     #
     #   
     #
-    my_Subproblem = ClassNQueensSD(N_RowsColumnsQueens)
+    my_Subproblem_NotInUse = ClassNQueensSD(N_RowsColumnsQueens)
     my_PartialSolution = ClassNQueensSD(N_RowsColumnsQueens)
     output_Results = []
-    NQueens_Helper(my_Subproblem, my_PartialSolution, output_Results)
+    i_row = 0
+    NQueens_Helper(my_Subproblem_NotInUse, i_row, my_PartialSolution, output_Results)
     return output_Results
 
-def NQueens_Helper(par_Subproblem, par_PartialSolution, par_Results):
+
+def NQueens_Helper(par_Subproblem_NotUsed, par_iRowIndex, par_PartialSolution, par_Results):
     # Added 3/18/2020 thomas downes
     #
     # Added 3/17/2020 thomas downes
     #
     # par_SubProblem = ClassNQueensSD(8)
     # par_PartialSolution = ClassNQueensSD(8) 
+
+    #
+    # For the incrementation, see "(par_iRowIndex + 1)" below. 
+    #
+    par_PartialSolution.RowIndex = par_iRowIndex
 
     #
     #  Backtracking Case  
@@ -40,7 +47,10 @@ def NQueens_Helper(par_Subproblem, par_PartialSolution, par_Results):
     if (par_PartialSolution.Completed()):
         # Output the full solution.
         # par_Results.append(par_PartialSolution.OutputArray())
-        par_Results.append(par_PartialSolution)
+        # par_Results.append(par_PartialSolution)
+        # par_Results.append(par_PartialSolution.Output_ChessboardString())
+        strChessboard = par_PartialSolution.Output_ChessboardString()
+        par_Results.append(strChessboard)
         return
 
     #
@@ -48,24 +58,62 @@ def NQueens_Helper(par_Subproblem, par_PartialSolution, par_Results):
     #
     boolAttack = False
     intCountColumnsOkay = 0
+    
+    ##intN_ProblemSize = par_Subproblem.N_GeneralProblemSize
+    intN_ProblemSize = par_PartialSolution.N_GeneralProblemSize
 
-    for iColIndex in range(par_Subproblem.N_GeneralProblemSize):
+    ##
+    ## Let's account for the fact that some rows might not allow 
+    ##    any Queens, due to a small problem size &/or all the diagonals 
+    ##    being occupied. 
+    ##
+    numRowsLeftToCheck = (par_PartialSolution.N_numRows - par_iRowIndex)
+    for iRowTry in range(numRowsLeftToCheck - 1):
         #
-        # Recursive Call
+        #Increment the Row Index.
         #
-        boolAttack = par_PartialSolution.AttackDetected_nextColumn(iColIndex)
-        if (False == boolAttack):
+        curr_iRowIndex = (par_iRowIndex + iRowTry)
+        par_PartialSolution.RowIndex = curr_iRowIndex
+
+        #
+        # Check each column. 
+        #
+        for iColIndex in range(intN_ProblemSize):  ## range(par_Subproblem.N_GeneralProblemSize):
             #
-            # No Queen attacks are detected.  We can proceed
-            #    with the recursive call. 
+            # Recursive Call
             #
-            # This the top piece of bread of the "Omkar's Sandwich" (Interview Kickstart)
-            boolItIsOkay = par_PartialSolution.setColumnChoice_isItOkay(iColIndex)
-            if (boolItIsOkay):
-                intCountColumnsOkay += 1
-                NQueens_Helper(par_Subproblem, par_PartialSolution, par_Results)
-                # This the bottom piece of bread of the "Omkar's Sandwich" (Interview Kickstart)
-                par_PartialSolution.removeLastColumnChoice()
+            boolAttack = par_PartialSolution.AttackDetected_nextColumn(iColIndex)
+
+            if (False == boolAttack):
+                #
+                # No Queen attacks are detected.  We can proceed
+                #    with the recursive call. 
+                #
+                # This the top piece of bread of the "Omkar's Sandwich" (Interview Kickstart)
+                boolItIsOkay = par_PartialSolution.setColumnChoice_isItOkay(iColIndex)
+                if (boolItIsOkay):
+                    intCountColumnsOkay += 1
+                    ## NQueens_Helper(par_Subproblem_NotUsed, par_PartialSolution, par_Results)
+                    NQueens_Helper(par_Subproblem_NotUsed, (curr_iRowIndex + 1), par_PartialSolution, par_Results)
+                    # This the bottom piece of bread of the "Omkar's Sandwich" (Interview Kickstart)
+                    par_PartialSolution.removeLastColumnChoice(curr_iRowIndex + 1)
+                else:
+                    pass
+            else:
+                pass
+
+        if (0 < intCountColumnsOkay):
+            # We have found a column to fill with a Queen, so we can stop the search.
+            break
+
+    #
+    # If (despite iterating through the remaining rows) no Queens could be placed in 
+    #   any of the remaining rows, output the Chessboard.
+    #
+    if (0 == intCountColumnsOkay):
+        strChessboard = par_PartialSolution.Output_ChessboardString()
+        par_Results.append(strChessboard)
+
     return 
 
 #
@@ -79,10 +127,13 @@ print("***      The N-Queens Problem      ***")
 print("**************************************")
 print("                                      ")
 
-Results = GetSolutions_NQueens_Overall(4)
+##Results = GetSolutions_NQueens_Overall(4)
+##--Results = GetSolutions_NQueens_Overall(1)
+Results = GetSolutions_NQueens_Overall(2)
 
-for each_ClassNQueens in Results:
-    print(each_ClassNQueens.Output_MasterString())
+for each_strChessboard in Results:
+    ## print(each_ClassNQueens.Output_MasterString())
+    print(each_strChessboard)
 
 
 
