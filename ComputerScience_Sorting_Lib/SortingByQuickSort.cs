ed87d59978468f4b8bc338bb487f56e8088985fa
tdownes57 <tomdownes1@gmail.com>
2020-09-19 21:30:46 -0700

    Completed a careful programming of QuickSort.

--- ComputerScience_Sorting_Lib/SortingByQuickSort.cs
+++ ComputerScience_Sorting_Lib/SortingByQuickSort.cs
@@ -24,7 +24,7 @@ namespace ComputerScience_Sorting_Lib
             //    https://sorting.at   
             //
 
-            return Sort_ReturnMilliseconds(param_array, True, False);
+            return Sort_ReturnMilliseconds(param_array, true, false);
 
         }
 
@@ -35,48 +35,99 @@ namespace ComputerScience_Sorting_Lib
             //
             //    https://sorting.at   
             //
+            bool bTrivialCase = false;  
+            bool bErrorOccurred = false;
+            string sErrorMessage = "";
 
+            const bool c_boolHoaresPartitioning = true;
+            const bool c_bPivotValueCanBeFirstItem = true; 
+            
+            if (c_boolHoaresPartitioning && pboolSortInput)
+            {
+                int intIndexOfPivot = -1;
+                if (c_boolHoaresPartitioning) intIndexOfPivot = 0;
+                else System.Diagnostics.Debugger.Break();
 
-
-
-
-
+                //
+                //----Major call !!  
+                //
+                Partitioning_by_Hoare(param_array, 0, -1 + param_array.Length, 
+                    intIndexOfPivot, ref bTrivialCase, ref bErrorOccurred, ref sErrorMessage);           
+            }
 
             return 0;
 
-
         }
 
-        public void Partitioning_by_Hoare(int[] param_array, int par_indexOfPivot)
+        public void Partitioning_by_Hoare(int[] param_array, 
+                                          int par_indexStart, 
+                                          int par_indexEnd, 
+                                          int par_indexOfPivot, 
+                                          ref bool par_bTrivialCase, 
+                                          ref bool par_bErrorOccurred,
+                                          ref string par_sErrorMessage)
         {
             //
             // Renamed from "Sort_InPlace_ByPivotIndex".  
             //
             // Added 3/12/2020 thomas downes 
             //
-            //      Let's use Lamuto's Partitioning / Two-Pointer Approach   
+            // Let's use Hoare's Partitioning / Two-Pointer Approach  
             //
+            //      (Lamuto's Partitioning will be a different procedure.)    
+            //
+            if (1 == param_array.Length) par_bTrivialCase = true;
             if (1 == param_array.Length) return;
+
+            if (par_indexStart == par_indexEnd) par_bTrivialCase = true;
+            if (par_indexStart == par_indexEnd) return;
+
+            if (par_indexStart > par_indexEnd) par_bErrorOccurred = true;
+            if (par_indexStart > par_indexEnd) par_sErrorMessage = "par_indexStart > par_indexEnd";
+            if (par_indexStart >= par_indexEnd) return;
+
+            if (par_indexOfPivot > par_indexEnd) par_bErrorOccurred = true;
+            if (par_indexOfPivot > par_indexEnd) par_sErrorMessage = "par_indexOfPivot > par_indexEnd";
+            if (par_indexOfPivot > par_indexEnd) return;
+
+            if (par_indexOfPivot < par_indexStart) par_bErrorOccurred = true;
+            if (par_indexOfPivot < par_indexStart) par_sErrorMessage = "par_indexOfPivot < par_indexStart";
+            if (par_indexOfPivot < par_indexStart) return;
+
+            if (par_indexEnd >= param_array.Length) par_bErrorOccurred = true;
+            if (par_indexEnd >= param_array.Length) par_sErrorMessage = "par_indexEnd >= param_array.Length";
+            if (par_indexEnd >= param_array.Length) return;
 
             int intPivotValue = param_array[par_indexOfPivot];
             int intCurrentPivotIndex = par_indexOfPivot;
             //int intInnerPointer = 0;
-            bool bLeftOfPivot;
-            bool bRightOfPivot;
+            //bool bLeftOfPivot;
+            //bool bRightOfPivot;
 
             //
-            //  Ironically, we take the pivot __out__ of the bulk of the array. 
+            //  Ironically, we take the pivot __out__ of the inner bulk of the array. 
             //
             //  Put the Pivot at the leftmost lefthand position of the array. 
             //
             //      (Swap the Pivot Value at the position 0.) 
             //
-            int intTempValue = param_array[0];
-            param_array[0] = param_array[intCurrentPivotIndex];
-            param_array[intCurrentPivotIndex] = intTempValue;
+            //-----int intTempValue = param_array[0];
+            if (par_indexOfPivot != par_indexStart)
+            {
+                //  Put the Pivot Item (Value) at the first, leftmost position of the sub-array
+                //    defined by par_indexStart and par_indexEnd. 
+                //    ----9/19/2020
+                int intTempValue = param_array[par_indexStart];
+                param_array[par_indexStart] = param_array[intCurrentPivotIndex];
+                param_array[intCurrentPivotIndex] = intTempValue;
+                //We have swapped the values, so the Pivot Index
+                //   becomes the starting index of the sub-array. 
+                intCurrentPivotIndex = par_indexStart;
+            }
 
             //intCurrentPivotIndex = 0;
-            bool bKeepLooping = True;
+            bool bKeepLooping = true;
+            bool bPointersCrossed = false;
 
             //
             //  Let's begin Hoare's Partioning.   
@@ -88,10 +139,20 @@ namespace ComputerScience_Sorting_Lib
             //
             // for (int intPointerIndex = 1; intPointerIndex <= -1 + param_array.Length; intPointerIndex++)
 
-            int intPointerIndex_I = 1;
-            int intPointerIndex_K = -1 + param_array.Length;
-            bool bNeedToThrow_LeftToRight = False;
-            bool bNeedToThrow_RightToLeft = False; 
+            //int intPointerIndex_I = 1;
+            //int intPointerIndex_K = -1 + param_array.Length;
+            //int intPointerIndex_I = par_indexStart + 1;
+            int intPointerIndex_I = (intCurrentPivotIndex + 1);
+            int intPointerIndex_K = par_indexEnd;
+
+            bool boolLetsIncrementPointer_Left = false;
+            bool boolLetsDecrementPointer_Right = false; 
+
+            bool bNeedToThrow_LeftToRight = false;
+            bool bNeedToThrow_RightToLeft = false;
+
+            bool bItemIs_GreaterThanPivot = false;
+            bool bItemIs_LessThanOrEqualToPivot = false;
 
             do
             {
@@ -114,62 +175,88 @@ namespace ComputerScience_Sorting_Lib
                 //Find the first lefthand item which should be "thrown" to the right,
                 //   i.e. from left side to the right side). 
                 //
+                bNeedToThrow_LeftToRight = false; //Reinitialize; 
                 do
                 {
-                    intPointerIndex_I++;
+                    //----moved to bottom of loop---intPointerIndex_I++;
 
-                    //Unlikely to occur....
+                    if (boolLetsIncrementPointer_Left) intPointerIndex_I++;
+
+                    //Unlikely to occur, but possible....
                     if (intPointerIndex_I > -1 + param_array.Length) break;
+                    if (intPointerIndex_I > par_indexEnd) break;
 
-                    bNeedToThrow_LeftToRight = (param_array[intPointerIndex_I] > intPivotValue);
+                    bItemIs_GreaterThanPivot = (param_array[intPointerIndex_I] > intPivotValue);
+                    bNeedToThrow_LeftToRight = bItemIs_GreaterThanPivot;
 
-                    if (intPointerIndex_I > intPointerIndex_K) break;
+                    //The following isn't needed at first, but will be needed eventually.
+                    //----if (intPointerIndex_I > intPointerIndex_K) break;
+                    bPointersCrossed = (intPointerIndex_I > intPointerIndex_K);
 
-                } while (False == bNeedToThrow_LeftToRight);
+                    boolLetsIncrementPointer_Left = (false == (bPointersCrossed || bNeedToThrow_LeftToRight));
+
+                } while (boolLetsIncrementPointer_Left);  //---- (false == bNeedToThrow_LeftToRight);
 
 
                 //Have the two pointers croseed each other? 
                 bKeepLooping = (intPointerIndex_I < intPointerIndex_K);
-                if (False == bKeepLooping) break;
+                if (false == bKeepLooping) break;
 
 
                 //
                 //Find the first righthand item which should be "thrown" to the left (i.e. right-to-left). 
                 //
+                bNeedToThrow_RightToLeft = false; //Reinitialize; 
                 do
                 {
-                    intPointerIndex_K--;
-                    
+                    //----moved to bottom of loop---intPointerIndex_K--;
+                    if (boolLetsDecrementPointer_Right) intPointerIndex_K--;
+
                     //Unlikely to occur....
                     if (intPointerIndex_K < 1) break;
+                    if (intPointerIndex_K < par_indexStart) break;
 
-                    bNeedToThrow_RightToLeft = (param_array[intPointerIndex_K] < intPivotValue);
+                    //bNeedToThrow_RightToLeft = (param_array[intPointerIndex_K] < intPivotValue);
+                    bItemIs_LessThanOrEqualToPivot = (param_array[intPointerIndex_K] <= intPivotValue);
+                    bNeedToThrow_RightToLeft = bItemIs_LessThanOrEqualToPivot;
 
-                    //if (intPointerIndex_K < intPointerIndex_I) break;
+                    //The following isn't needed at first, but will be needed eventually.
+                    //----if (intPointerIndex_I > intPointerIndex_K) break;
+                    bPointersCrossed = (intPointerIndex_I > intPointerIndex_K);
+
+                    boolLetsDecrementPointer_Right = (false == (bPointersCrossed || bNeedToThrow_RightToLeft));
+
+                } while (boolLetsDecrementPointer_Right);  //----(false == bNeedToThrow_LeftToRight);
+
+                const bool c_checkPointers = false;
+                if (c_checkPointers)
+                {
+                    //Unlikely to be needed....
+                    bKeepLooping = (intPointerIndex_I <= -1 + param_array.Length);
+                    if (false == bKeepLooping) break;
+                    //Unlikely to be needed....
+                    bKeepLooping = (intPointerIndex_K >= 1);
+                    if (false == bKeepLooping) break;
+                    //Have the two pointers croseed each other? 
                     bKeepLooping = (intPointerIndex_I < intPointerIndex_K);
-                    if (bKeepLooping) break;
-
-                } while (False == bNeedToThrow_LeftToRight);
-
-                //Unlikely to occur....
-                bKeepLooping = (intPointerIndex_I <= -1 + param_array.Length);
-                if (False == bKeepLooping) break;
-                //Unlikely to occur....
-                bKeepLooping = (intPointerIndex_K >= 1);
-                if (False == bKeepLooping) break;
-
-                //Have the two pointers croseed each other? 
-                bKeepLooping = (intPointerIndex_I < intPointerIndex_K);
-                if (False == bKeepLooping) break;
+                    if (false == bKeepLooping) break;
+                }
 
                 //
                 //  Swap / Throw
                 //
-                int intThrow_LeftToRight = param_array[intPointerIndex_I];
-                int intThrow_RightToLeft = param_array[intPointerIndex_K];
-                
-                param_array[intPointerIndex_I] = intThrow_RightToLeft;
-                param_array[intPointerIndex_K] = intThrow_LeftToRight;
+                if (bNeedToThrow_LeftToRight && bNeedToThrow_RightToLeft)
+                {
+                    int intThrow_LeftToRight = param_array[intPointerIndex_I];
+                    int intThrow_RightToLeft = param_array[intPointerIndex_K];
+
+                    param_array[intPointerIndex_I] = intThrow_RightToLeft;
+                    param_array[intPointerIndex_K] = intThrow_LeftToRight;
+
+                    boolLetsIncrementPointer_Left = true;
+                    boolLetsDecrementPointer_Right = true;
+                }
+                else bKeepLooping = false;
 
             } while (bKeepLooping);
 
@@ -177,9 +264,38 @@ namespace ComputerScience_Sorting_Lib
             //Place the Pivot Value where it should go, i.e. somewhere
             //   in the midst of the array (vs. in the 0th position). 
             //
-            int intLessThanPivot = param_array[intPointerIndex_K];
-            param_array[intPointerIndex_K] = intPivotValue;
-            param_array[0] = intLessThanPivot;
+            int intLessThanPivot;
+            const bool c_bMovePivotToPointerK = false;
+            if (c_bMovePivotToPointerK)
+            {
+                intLessThanPivot = param_array[intPointerIndex_K];
+                param_array[intPointerIndex_K] = intPivotValue;
+                param_array[intCurrentPivotIndex] = intLessThanPivot;
+                //Refresh the location of the Pivot.
+                intCurrentPivotIndex = intPointerIndex_K;
+            }
+            else
+            {
+                intLessThanPivot = param_array[intPointerIndex_I];
+                param_array[intPointerIndex_I] = intPivotValue;
+                param_array[intCurrentPivotIndex] = intLessThanPivot;
+                //Refresh the location of the Pivot.
+                intCurrentPivotIndex = intPointerIndex_I;
+            }
+
+            //
+            //---Recursive call !!   Left-hand branch. 
+            //
+            int intNextPivotIndex = par_indexStart;
+            Partitioning_by_Hoare(param_array, par_indexStart, -1 + intCurrentPivotIndex,
+                intNextPivotIndex, ref pbTrivialCase, ref pbErrorOccurred, ref psErrorMessage);
+
+            //
+            //---Recursive call !!   Right-hand branch. 
+            //
+            intNextPivotIndex = +1 + intCurrentPivotIndex;
+            Partitioning_by_Hoare(param_array, +1 + intCurrentPivotIndex, par_indexEnd,
+                intNextPivotIndex, ref bTrivialCase, ref bErrorOccurred, ref sErrorMessage);
 
             //
             //We have completed our work.  
@@ -187,5 +303,19 @@ namespace ComputerScience_Sorting_Lib
             return;
 
         }
+
+        public void Partitioning_by_Lamuto(int[] param_array, int par_indexOfPivot)
+        {
+            //
+            // Renamed from "Sort_InPlace_ByPivotIndex".  
+            //
+            // Added 3/12/2020 thomas downes 
+            //
+            //      Let's use Lamuto's Partitioning / Two-Pointer Approach   
+            //
+            if (1 == param_array.Length) return;
+
+      
+
+        }
     }
-}
